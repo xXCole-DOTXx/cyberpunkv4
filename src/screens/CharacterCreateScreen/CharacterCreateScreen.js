065a80017568cbd8d686fad57b6d71121425a7e2
Cole Perry <perry391@marshall.edu>
2020-12-02 21:04:48 -0500

    Added skill points and scrollview

--- src/screens/CharacterCreateScreen/CharacterCreateScreen.js
+++ src/screens/CharacterCreateScreen/CharacterCreateScreen.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import { unstable_renderSubtreeIntoContainer } from 'react-dom';
-import { Text, View, TextInput, Button, Image, StyleSheet, TouchableOpacity, Picker} from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Button, Picker, ScrollView} from 'react-native'
+import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native';
 import * as firebase from '../../firebase'; 
 
 const CharacterCreateScreen = ({navigation, route}) =>{
@@ -50,16 +50,17 @@ const CharacterCreateScreen = ({navigation, route}) =>{
 
 
     return (
+      <ScrollView>
         <View style={styles.container}>
             <Text>Character Screen</Text>
-
+            <Text>You have {skillPoints} remaining.</Text>
             <TextInput
             style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 5 }}
             placeholder="Name"
             onChangeText={(val) => setName(val)}
             />
 
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 5}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 5, padding: 10}}>
                     <Picker
                     selectedValue={selectedValue}
                     style={{ height: 50, width: 150 }}
@@ -78,8 +79,289 @@ const CharacterCreateScreen = ({navigation, route}) =>{
                     
                     </Picker>
                 </View>
-
                 
+      <View style={{flexDirection: 'col', justifyContent: 'space-between', margin: 5}}>
+        {/* Int buttons */}
+        <Text style={{margin: 5}}>
+          <Button 
+              title="+"
+                      onPress={() => {
+            if(skillPoints > 0 && charInt < 10){
+            setInt(charInt + 1);
+            setPoints(skillPoints - 1);
+            }
+            else{
+              setInt(charInt);
+            }
+          }}
+          />
+          
+          Int: {charInt}
+        
+          <Button 
+              title="-"
+                      onPress={() => {
+            if(charInt > 0){
+            setInt(charInt - 1);
+            setPoints(skillPoints + 1);
+            }
+            else{
+              setInt(charInt);
+            }
+          }}
+          />
+        </Text>
+
+        {/* Ref buttons */}
+      <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0 && charRef < 10){
+          setRef(charRef + 1);
+          setPoints(skillPoints - 1);
+          }
+          else{
+            setRef(charRef);
+          }
+        }}
+        />
+        
+        Ref: {charRef}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charRef > 0){
+          setRef(charRef - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setRef(charRef);
+          }
+        }}
+        />
+      </Text>
+
+      {/* Tech buttons */}
+      <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0 && charTech < 10){
+          setTech(charTech + 1);
+          setPoints(skillPoints - 1);
+          }
+          else{
+            setTech(charTech);
+          }
+        }}
+        />
+        
+        Tech: {charTech}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charTech > 0){
+          setTech(charTech - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setTech(charTech);
+          }
+        }}
+        />
+      </Text>
+
+      {/* Cool buttons */}
+      <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0 && charCool < 10){
+          setCool(charCool + 1);
+          setPoints(skillPoints - 1);
+          }
+          else{
+            setCool(charCool);
+          }
+        }}
+        />
+        
+        Cool: {charCool}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charCool > 0){
+          setCool(charCool - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setCool(charCool);
+          }
+        }}
+        />
+      </Text>
+
+      {/* Attr buttons */}
+      <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0 && charAttr < 10){
+          setAttr(charAttr + 1);
+          setPoints(skillPoints -1 );
+          }
+          else{
+            setAttr(charAttr);
+          }
+        }}
+        />
+        
+        Attr: {charAttr}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charAttr > 0){
+          setAttr(charAttr - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setAttr(charAttr);
+          }
+        }}
+        />
+      </Text>
+
+      {/* Luck buttons */}
+      <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0 && charLuck < 10){
+          setLuck(charLuck + 1);
+          setPoints(skillPoints - 1);
+          }
+          else{
+            setLuck(charLuck);
+          }
+        }}
+        />
+        
+        Luck: {charLuck}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charLuck > 0){
+          setLuck(charLuck - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setLuck(charLuck);
+          }
+        }}
+        />
+      </Text>
+
+      {/* MA buttons */}
+      <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0 && charMa < 10){
+          setMa(charMa + 1);
+          setPoints(skillPoints - 1);
+          }
+          else{
+            setMa(charMa);
+          }
+        }}
+        />
+        
+        MA: {charMa}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charMa > 0){
+          setMa(charMa - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setMa(charMa);
+          }
+        }}
+        />
+      </Text>
+
+       {/* Body buttons */}
+       <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0){
+          setBody(charBody + 1 && charBody < 10);
+          setPoints(skillPoints - 1);
+          }
+          else{
+            setBody(charBody);
+          }
+        }}
+        />
+        
+        Body: {charBody}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charBody > 0){
+          setBody(charBody - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setBody(charBody);
+          }
+        }}
+        />
+      </Text>
+
+      {/* Emp buttons */}
+      <Text style={{margin: 5}}>
+        <Button 
+            title="+"
+                    onPress={() => {
+          if(skillPoints > 0 && charEmp < 10){
+          setEmp(charEmp + 1);
+          setPoints(skillPoints - 1);
+          }
+          else{
+            setEmp(charEmp);
+          }
+        }}
+        />
+        
+        Emp: {charEmp}
+      
+        <Button 
+            title="-"
+                    onPress={() => {
+          if(charEmp > 0){
+          setEmp(charEmp - 1);
+          setPoints(skillPoints + 1);
+          }
+          else{
+            setEmp(charEmp);
+          }
+        }}
+        />
+      </Text>
+
+
+      </View>
 
             <Button
                 title="Submit"
@@ -89,19 +371,20 @@ const CharacterCreateScreen = ({navigation, route}) =>{
             />
 
         </View>
+    </ScrollView>
     )
 }
 
 //Character Creation stylesheet
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      marginBottom: 10
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginBottom: 10
+  },
+});
 
 
 export default CharacterCreateScreen;
