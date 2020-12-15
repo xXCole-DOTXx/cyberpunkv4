3b5b698da2f3701ead17cacf9128f8282e5a1a71
Cole Perry <E096752@wv.gov>
2020-12-15 12:21:42 -0500

    Button added to character select

--- src/screens/CharacterSelectScreen/CharacterSelectScreen.js
+++ src/screens/CharacterSelectScreen/CharacterSelectScreen.js
@@ -4,33 +4,58 @@ import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/
 import { userFetch } from '../../sharedComponents/userFetch.js';
 
 const CharacterSelectScreen = ({navigation, route}) => {
-  const [results, setResults] = useState([]);
+  const [characterList, setCharacterList] = useState([]);
   const [selectedValue, setSelectedValue] = useState("Choose..");
 
+  //Using React.useEffect to simulate React's #componentDidMount
        React.useEffect(()=>{
         console.log('Select Screen loaded.')
+        //Must use an async function to call the userFetch function then set the result equal to characterList
         const asyncFunc = async () => {
-          const response = await userFetch();
-          setResults(response);
+          const charList = await userFetch();
+          setCharacterList(charList);
         }
+        //Call the async function
         asyncFunc();
       },[])
 
     return (
-        <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+        <View style={styles.container}>
             <Text>Select a character:</Text>
             <Picker
+              style={styles.charPicker}
               mode="dropdown"
               selectedValue={selectedValue}
               onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
               >
-              {results.map((item, index) => {
-                  return (<Picker.Item label={item} value={index} key={index}/>) 
+              {characterList.map((item, index) => {
+                  return (<Picker.Item label={item} value={item} key={index}/>) 
               })}
           </Picker>
+          <Button
+                title="Submit"
+                        onPress={() => {
+                            console.log(selectedValue);
+                        }}
+                color="#19AC52"
+            />
       </View>
     )
 }
+
+//Character Select stylesheet
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1,
+    justifyContent: 'flex-start' 
+  },
+
+  charPicker: {
+    flex: 2, 
+    justifyContent: 'flex-start' 
+  }
+
+});
 
 
 export default CharacterSelectScreen;
