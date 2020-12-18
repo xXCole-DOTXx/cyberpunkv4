a61a92f78ee132d0b8fa1960bd2196124264bb57
Cole Perry <perry391@marshall.edu>
2020-12-18 15:30:22 -0500

    Getting player info working

--- src/screens/CharacterSkillScreen/CharacterSkillScreen.js
+++ src/screens/CharacterSkillScreen/CharacterSkillScreen.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Button, Picker, ScrollView} from 'react-native'
 import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native';
-import { userFetch } from '../../sharedComponents/userFetch.js';
 import { styles } from './styles.js';
 import { getCharInfo } from '../../sharedComponents/getCharInfo.js';
 
 const CharacterSkillScreen = ({navigation, route}) => {
     //Getting params passed from HomeScreen
     const { playerName } = route.params;
-    const [player, setPlayer] = useState([]);
+    const [player, setPlayer] = useState();
 
      //Using React.useEffect to simulate React's #componentDidMount
      React.useEffect(()=>{
@@ -17,21 +16,28 @@ const CharacterSkillScreen = ({navigation, route}) => {
         const asyncFunc = async () => {
           const character = await getCharInfo(playerName);
           setPlayer(character);
-          console.log(character.data().Role);
         }
         //Call the async function
         asyncFunc();
       },[])
   
+
     return (
         <View style={styles.container}>
-            <Text>Player Name: {playerName}</Text>
-            <Text>Player Role: {player.data().Role}</Text>
+            {player?.data()?.userName && <Text>{player?.data()?.userName}</Text>}
 
             <View style={styles.stats}>
-              <Text>Int: {player.data().stats.Int}</Text>
-              <Text>Ref: {player.data().stats.Ref}</Text>
-              <Text>Cool: {player.data().stats.Cool}</Text>
+              {player?.data()?.stats && <Text>Int: {player?.data()?.stats.Int}</Text>}
+              {player?.data()?.stats && <Text>Ref: {player?.data()?.stats.Ref}</Text>}
+              {player?.data()?.stats && <Text>Tech: {player?.data()?.stats.Tech}</Text>}
+              {player?.data()?.stats && <Text>Cool: {player?.data()?.stats.Cool}</Text>}
+            </View>
+            <View style={styles.stats}>
+              {player?.data()?.stats && <Text>Attr: {player?.data()?.stats.Attr}</Text>}
+              {player?.data()?.stats && <Text>Luck: {player?.data()?.stats.Luck}</Text>}
+              {player?.data()?.stats && <Text>MA: {player?.data()?.stats.MA}</Text>}
+              {player?.data()?.stats && <Text>Body: {player?.data()?.stats.Body}</Text>}
+              {player?.data()?.stats && <Text>Emp: {player?.data()?.stats.Emp}</Text>}
             </View>
         </View>
     )
