4ffd163909753474c61678cb4a4d3c41eaa8a1f9
Cole Perry <E096752@wv.gov>
2021-01-04 12:23:13 -0500

    Using yarn now

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import CharacterCreateScreen from './src/screens/CharacterCreateScreen/CharacterCreateScreen';
 import CharacterSelectScreen from './src/screens/CharacterSelectScreen/CharacterSelectScreen';
@@ -13,6 +14,7 @@ import * as firebase from './src/firebase';
 
 //This app currently uses stateless function components :: https://stackoverflow.com/questions/57762163/const-vs-class-functions-is-react-native
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
 
@@ -28,17 +30,23 @@ export default function App() {
   //Initialize database
   dbInit();
 
+  createHomeStack = () =>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+          <Stack.Screen name="CharacterCreate" component={CharacterCreateScreen}></Stack.Screen>
+          <Stack.Screen name="CharacterSelect" component={CharacterSelectScreen}></Stack.Screen>
+          <Stack.Screen name="CharacterSkillScreen" component={CharacterSkillScreen}></Stack.Screen>
+          <Stack.Screen name="SkillScreen" component={SkillScreen}></Stack.Screen>
+          <Stack.Screen name="CareerPointsScreen" component={CareerPointsScreen}></Stack.Screen>
+          <Stack.Screen name="RockerboyCareerScreen" component={RockerboyCareerScreen}></Stack.Screen>
+        </Stack.Navigator>
+
   return (
     <NavigationContainer initialRouteName ='Home'>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
-        <Stack.Screen name="CharacterCreate" component={CharacterCreateScreen}></Stack.Screen>
-        <Stack.Screen name="CharacterSelect" component={CharacterSelectScreen}></Stack.Screen>
-        <Stack.Screen name="CharacterSkillScreen" component={CharacterSkillScreen}></Stack.Screen>
-        <Stack.Screen name="SkillScreen" component={SkillScreen}></Stack.Screen>
-        <Stack.Screen name="CareerPointsScreen" component={CareerPointsScreen}></Stack.Screen>
-        <Stack.Screen name="RockerboyCareerScreen" component={RockerboyCareerScreen}></Stack.Screen>
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={createHomeStack} />
+        <Drawer.Screen name="Users" component={CharacterSelectScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
