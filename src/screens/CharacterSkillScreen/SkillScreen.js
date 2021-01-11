285eecf1f53e69b1eccd8307b75a597d0c3dbb63
Cole Perry <perry391@marshall.edu>
2021-01-10 19:18:17 -0500

    Skill screen coming along

--- src/screens/CharacterSkillScreen/SkillScreen.js
+++ src/screens/CharacterSkillScreen/SkillScreen.js
@@ -35,25 +35,145 @@ const SkillScreen = ({navigation, route}) => {
       </View>
     )
   } else {
-     const {userName, Role} = player;
+     const {
+       userName, 
+       Role,
+       stats: {
+          Attr,
+          Body,
+          Cool,
+          Emp,
+          Int,
+          Luck,
+          MA,
+          Ref,
+          Tech
+       },
+      Attractivness : {
+        Personal_Grooming,
+        Wardrobe_Style
+      },
+      Body : {
+        Endurance,
+        Strength_Feat,
+        Swimming
+      },
+      Cool : {
+        Interrogation,
+        Intimidate,
+        Oratory,
+        Resist_Torture_Drugs,
+        Streetwise
+      },
+      Empathy : {
+        Human_Perception,
+        Interview,
+        Leadership,
+        Perform,
+        Persuasion_Fast_Talk,
+        Seduction,
+        Social
+      },
+      Intelligence : {
+        Accounting,
+        Anthopology,
+        Awareness_Notice,
+        Biology,
+        Botany,
+        Chemistry,
+        Composition,
+        Diagnose_Illness,
+        Education_Gen_Knowledge,
+        Expert,
+        Gamble,
+        Geology,
+        Hide_Evade,
+        History,
+        Language1,
+        Language2,
+        Language3,
+        Library_Search,
+        Mathematics,
+        Physics,
+        Programming,
+        Shadow_Track,
+        Stock_Market,
+        System_Knowledge,
+        Teaching,
+        Wilderness_Survival,
+        Zoology
+      },
+      Reflex : {
+        Archery,
+        Athletics,
+        Brawling,
+        Dance,
+        Dodge_Escape,
+        Driving,
+        Fencing,
+        Handgun,
+        Heavy_Weapons,
+        Martial_Art1,
+        Martial_Art2,
+        Martial_Art3,
+        Melee,
+        Motorcycle,
+        Operate_Hvy_Machinery,
+        Pilot_Dirigible,
+        Pilot_Fixed_Wings,
+        Pilot_Gyro,
+        Pilot_Vect_Thrust_Vehicle,
+        Rifle,
+        Stealthm,
+        Submachinegun
+      },
+      Spcial_Abilities : {
+        Authority,
+        Charismatic_Leadership,
+        Combat_Sense,
+        Credibility,
+        Family,
+        Interface,
+        Jury_Rig,
+        Medical_Tech,
+        Resources,
+        Streetdeal
+      },
+      Technology : {
+        AV_Tech,
+        Aero_Tech,
+        Basic_Tech,
+        Cryotank_Operation,
+        CyberTech,
+        Cyberdeck_Design,
+        Demolitions,
+        Disguise,
+        Electric_Security,
+        Electronics,
+        First_Aid,
+        Forgery,
+        Gyro_Tech,
+        Paint_Draw,
+        Pharmacuticals,
+        Photo_Film,
+        Pick_Lock,
+        Pick_Pocket,
+        Play_Instrument,
+        Weaponsmith
+      }
+    } = player;
      return (
         <ScrollView>
           <View>  
             <Text>{userName}</Text>
-           
+            <Text>{Role}</Text>
+            <Text>{Int}</Text>
+            <Text>Weaponsmith: {Weaponsmith}</Text>
           </View>
         </ScrollView>
       )
   }
-  
-   return (
-        <ScrollView>
-          <View>  
-          
-           
-          </View>
-        </ScrollView>
-      )
+
 }
 
 export default SkillScreen;
