ef37d4bb8c61941417b6aeab32f4e114bde31db8
Cole Perry <perry391@marshall.edu>
2021-01-10 20:28:44 -0500

    Skill screen working and fully destructured

--- src/screens/CharacterSkillScreen/SkillScreen.js
+++ src/screens/CharacterSkillScreen/SkillScreen.js
@@ -124,7 +124,7 @@ const SkillScreen = ({navigation, route}) => {
         Pilot_Gyro,
         Pilot_Vect_Thrust_Vehicle,
         Rifle,
-        Stealthm,
+        Stealth,
         Submachinegun
       },
       Spcial_Abilities : {
@@ -164,11 +164,138 @@ const SkillScreen = ({navigation, route}) => {
     } = player;
      return (
         <ScrollView>
-          <View>  
-            <Text>{userName}</Text>
-            <Text>{Role}</Text>
-            <Text>{Int}</Text>
-            <Text>Weaponsmith: {Weaponsmith}</Text>
+          <View>
+            <List.Section title={userName + ": " + Role}>
+
+              <List.Accordion
+                title="Special Abilities">
+                <List.Item title={"Authority: " + Authority} />
+                <List.Item title={"Charismatic Leadership: " + Charismatic_Leadership}/>
+                <List.Item title={"Combat Sense: " + Combat_Sense} />
+                <List.Item title={"Credibility: " + Credibility} />
+                <List.Item title={"Family: " + Family} />
+                <List.Item title={"Interface: " + Interface} />
+                <List.Item title={"Jury Rig: " + Jury_Rig} />
+                <List.Item title={"Medical Tech: " + Medical_Tech} />
+                <List.Item title={"Resources: " + Resources} />
+                <List.Item title={"Streetdeal: " + Streetdeal} />
+              </List.Accordion>
+
+              <List.Accordion
+                title={"Attractivness: " + Attr}>
+                <List.Item title={"Personal Grooming: " + Personal_Grooming}/>
+                <List.Item title={"Wardrobe & Style: " + Wardrobe_Style} />
+              </List.Accordion>
+
+              <List.Accordion
+                title={"Body: " + Body}>
+                <List.Item title={"Endurance: " + Endurance}/>
+                <List.Item title={"Strength Feat: " + Strength_Feat} />
+                <List.Item title={"Swimming: " + Swimming}/>
+              </List.Accordion>
+
+              <List.Accordion
+                title={"Cool: " + Cool}>
+                <List.Item title={"Interrogation: " + Interrogation}/>
+                <List.Item title={"Intimidate: " + Intimidate} />
+                <List.Item title={"Oratory: " + Oratory}/>
+                <List.Item title={"Resist Torture & Drugs: " + Resist_Torture_Drugs}/>
+                <List.Item title={"Streetwise: " + Streetwise}/>
+              </List.Accordion>
+
+              <List.Accordion
+                title={"Empathy: " + Emp}>
+                <List.Item title={"Human Perception: " + Human_Perception}/>
+                <List.Item title={"Interview: " + Interview} />
+                <List.Item title={"Leadership: " + Leadership}/>
+                <List.Item title={"Perform: " + Perform}/>
+                <List.Item title={"Persuasion & Fast Talk: " + Persuasion_Fast_Talk}/>
+                <List.Item title={"Seduction: " + Seduction}/>
+                <List.Item title={"Social: " + Social}/>
+              </List.Accordion>
+
+              <List.Accordion
+                title={"Intelligence: " + Int}>
+                <List.Item title={"Accounting: " + Accounting}/>
+                <List.Item title={"Anthropology: " + Anthopology} />
+                <List.Item title={"Awareness/Notice: " + Awareness_Notice}/>
+                <List.Item title={"Biology: " + Biology}/>
+                <List.Item title={"Botany: " + Botany}/>
+                <List.Item title={"Chemistry: " + Chemistry}/>
+                <List.Item title={"Composition: " + Composition}/>
+                <List.Item title={"Diagnose Illness: " + Diagnose_Illness}/>
+                <List.Item title={"Education & Gen Knowledge: " + Education_Gen_Knowledge}/>
+                <List.Item title={"Expert: " + Expert}/>
+                <List.Item title={"Gamble: " + Gamble}/>
+                <List.Item title={"Geology: " + Geology}/>
+                <List.Item title={"Hide/Evade: " + Hide_Evade}/>
+                <List.Item title={"History: " + History}/>
+                <List.Item title={"Language 1: " + Language1}/>
+                <List.Item title={"Language 2: " + Language2}/>
+                <List.Item title={"Language 3: " + Language3}/>
+                <List.Item title={"Library Search: " + Library_Search}/>
+                <List.Item title={"Mathematics: " + Mathematics}/>
+                <List.Item title={"Physics: " + Physics}/>
+                <List.Item title={"Programming: " + Programming}/>
+                <List.Item title={"Shadow/Track: " + Shadow_Track}/>
+                <List.Item title={"Stock_Market: " + Mathematics}/>
+                <List.Item title={"System Knowledge: " + System_Knowledge}/>
+                <List.Item title={"Teaching: " + Teaching}/>
+                <List.Item title={"Wilderness Survival: " + Wilderness_Survival}/>
+                <List.Item title={"Zoology: " + Zoology}/>
+              </List.Accordion>
+
+              <List.Accordion
+                title={"Reflex: " + Ref}>
+                <List.Item title={"Archery: " + Archery}/>
+                <List.Item title={"Athletics: " + Athletics} />
+                <List.Item title={"Brawling: " + Brawling}/>
+                <List.Item title={"Dance: " + Dance}/>
+                <List.Item title={"Dodge/Escape: " + Dodge_Escape}/>
+                <List.Item title={"Driving: " + Driving}/>
+                <List.Item title={"Fencing: " + Fencing}/>
+                <List.Item title={"Handgun: " + Handgun}/>
+                <List.Item title={"Heavy Weapons: " + Heavy_Weapons}/>
+                <List.Item title={"Martial Art 1: " + Martial_Art1}/>
+                <List.Item title={"Martial Art 2: " + Martial_Art2}/>
+                <List.Item title={"Martial Art 3: " + Martial_Art3}/>
+                <List.Item title={"Melee: " + Melee}/>
+                <List.Item title={"Motorcycle: " + Motorcycle}/>
+                <List.Item title={"Operate Hvy Machinery: " + Operate_Hvy_Machinery}/>
+                <List.Item title={"Pilot Dirigible: " + Pilot_Dirigible}/>
+                <List.Item title={"Pilot Fixed Wings: " + Pilot_Fixed_Wings}/>
+                <List.Item title={"Pilot Gyro: " + Pilot_Gyro}/>
+                <List.Item title={"Pilot Vect Thrust Vehicle: " + Pilot_Vect_Thrust_Vehicle}/>
+                <List.Item title={"Rifle: " + Rifle}/>
+                <List.Item title={"Stealth: " + Stealth}/>
+                <List.Item title={"Submachinegun: " + Submachinegun}/>
+              </List.Accordion>
+
+              <List.Accordion
+                title={"Technology: " + Tech}>
+                <List.Item title={"AV Tech: " + AV_Tech}/>
+                <List.Item title={"Aero Tech: " + Aero_Tech} />
+                <List.Item title={"Basic Tech: " + Basic_Tech}/>
+                <List.Item title={"Cryotank Operation: " + Cryotank_Operation}/>
+                <List.Item title={"Cybertech: " + CyberTech}/>
+                <List.Item title={"Cyberdeck Design: " + Cyberdeck_Design}/>
+                <List.Item title={"Demolitions: " + Demolitions}/>
+                <List.Item title={"Disguise: " + Disguise}/>
+                <List.Item title={"Electric Security: " + Electric_Security}/>
+                <List.Item title={"Electronics: " + Electronics}/>
+                <List.Item title={"First Aid: " + First_Aid}/>
+                <List.Item title={"Forgery: " + Forgery}/>
+                <List.Item title={"Gyro Tech: " + Gyro_Tech}/>
+                <List.Item title={"Paint/Draw: " + Paint_Draw}/>
+                <List.Item title={"Pharmacuticals: " + Pharmacuticals}/>
+                <List.Item title={"Photo/Film: " + Photo_Film}/>
+                <List.Item title={"Pick Lock: " + Pick_Lock}/>
+                <List.Item title={"Pick Pocket: " + Pick_Pocket}/>
+                <List.Item title={"Play Instrument: " + Play_Instrument}/>
+                <List.Item title={"Weaponsmith: " + Weaponsmith}/>
+              </List.Accordion>
+
+            </List.Section>
           </View>
         </ScrollView>
       )
